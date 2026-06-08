0e32eb5e39c50ff7a6e77de6f5b94027c28a2c1e
Mrerg7 <87950693+Mrerg7@users.noreply.github.com>
2026-06-08 07:03:40 -0700

    feat: add Google Site Verification meta tag

--- src/config/site.ts
+++ src/config/site.ts
@@ -7,6 +7,7 @@ export const SITE = {
   email: 'sales@desertrich.com',
   locale: 'en_US',
   location: 'Arizona',
+  googleSiteVerification: 'zy9vP-Yd7va2ltlIS1wiP26T5J35LKvYzrWq_cWKeso',
 } as const;
 
 export const CF_IMAGES = {
